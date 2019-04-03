7463a10485b2f77a65833a31b1eb011ad61aee30
anv6264 <anv.6264@gmail.com>
2019-04-03 23:59:28 +0300

    first commit

---
+++ js17.js
@@ -0,0 +1,42 @@
+var x = 3, y = 60, z = 60;
+
+console.log( x * y * z);
+
+
+var num = 1;
+
+num += 12;
+
+num -= 14;
+
+num *= 5;
+
+num /= 7;
+
+num++ ;
+
+num-- ;
+
+alert( num );
+
+
+var a = 10, b = 2, c = 5;
+
+alert(+a + +b + +c);
+
+
+var a = 17, b = 10;
+
+alert( c = a - b);
+
+
+var d = 7;
+
+var result = c + d;
+
+alert(result);
+
+
+var name = 'Nataliia';
+
+alert('Hello, Nataliia!');
